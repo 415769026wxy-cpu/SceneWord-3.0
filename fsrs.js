b086a4136b974b658eb970503df94de2f7509bb8
415769026wxy-cpu <415769026wxy@gmail.com>
2026-08-22 17:45:13 +0800

    Delete fsrs.js

--- fsrs.js
+++
@@ -1,7 +0,0 @@
-export function fsrsUpdate(card,rating){
-let stability=card.stability||1;
-if(rating>=4) stability*=2;
-else if(rating===3) stability*=1.2;
-else stability=1;
-return {...card,stability,nextReview:Date.now()+stability*86400000};
-}
